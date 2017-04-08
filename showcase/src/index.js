e2877e68aa63660379574ff38ae1a86be6100a06
embrah <emladenoff@gmail.com>
2017-04-08 11:11:19 +0100

    refactoring

---
+++ showcase/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Rebecca from './components/Rebecca';
+import './index.css';
+
+ReactDOM.render(
+  <Rebecca />,
+  document.getElementById('root')
+);
