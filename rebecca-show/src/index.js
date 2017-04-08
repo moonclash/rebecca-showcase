0aca22fb631b8cd863c034658cf14550c25434d4
embrah <emladenoff@gmail.com>
2017-04-08 11:12:16 +0100

    more refactoring

--- rebecca-show/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Rebecca from './components/Rebecca';
-import './index.css';
-
-render(
-  <Rebecca />,
-  document.getElementById('root')
-);
